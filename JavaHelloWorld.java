2f3274344cdc88348e5ae0505c1cf7739aa7314e
shashi K Verma <shashik.verma@gmail.com>
2019-12-04 12:17:04 +0530

    docker cicd fail

---
+++ JavaHelloWorld.java
@@ -0,0 +1,9 @@
+public class JavaHelloWorld
+{
+   public static void main(String[] args)
+   {
+     system.out.println("Hello World");
+     system.out.println("Java CI example");
+     system.out.println("Updated for docker build failure");
+   }
+}
